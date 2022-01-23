6264a24021552f88caa6ad5b7bcbc40c278d1c8d
Sanjana Nalawade <sanjanavnalawade@gmail.com>
2022-01-23 17:41:53 -0500

    first commit

---
+++ app/src/main/java/edu/neu/madcourse/currencyconverter/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.neu.madcourse.currencyconverter;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view){
+        EditText dollarText = findViewById(R.id.dollarText);
+        TextView textView = findViewById(R.id.textView);
+        if(!dollarText.getText().toString().equals("")){
+            float dollarValue = Float.parseFloat(dollarText.getText().toString());
+            float rupeeValue = dollarValue * 74.42F;
+            textView.setText(String.format(Locale.ENGLISH,"%f", rupeeValue));
+        } else {
+            textView.setText(R.string.no_value_string);
+        }
+    }
+}
